0191a5259217e1902283a709ffe48a709ba4638c
zestgeek7 <zestgeek7@zestgeek7s-MacBook-Air.local>
2020-11-25 10:50:50 +0530

    fixed the issues in the file

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-function count(main_str, sub_str) 
-{
+function count(main_str, sub_str){
 	main_str += '';
 	sub_str += '';
-
-	if (sub_str.length <= 0) 
-	{
+	if (sub_str.length <= 0){
 		return main_str.length + 1;
 	}
-
-	subStr = sub_str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	var subStr = sub_str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 	return (main_str.match(new RegExp(subStr, 'gi')) || []).length;
 }
 console.log(count("ABABAB", 'A'));
